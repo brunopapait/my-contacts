a259208e3f45f11c3fc9f3609805d49e027117d5
brunopapait <brunohpapait@gmail.com>
2021-07-18 22:18:04 -0300

    up ..

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Select from '../Select';
 import Input from '../Input';
@@ -6,19 +7,40 @@ import { Form, ButtonContainer } from './styles';
 import FormGroup from '../FormGroup';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  function changeName(e) {
+    setName(e.target.value);
+  }
+
+  function changeEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  function changePhone(e) {
+    setPhone(e.target.value);
+  }
+
+  function changeCategory(e) {
+    setCategory(e.target.value);
+  }
+
   return (
     <Form>
       <FormGroup>
-        <Input placeholder="Nome" />
+        <Input placeholder="Nome" value={name} onChange={changeName} />
       </FormGroup>
       <FormGroup>
-        <Input placeholder="E-mail" />
+        <Input placeholder="E-mail" value={email} onChange={changeEmail} />
       </FormGroup>
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input placeholder="Telefone" value={phone} onChange={changePhone} />
       </FormGroup>
       <FormGroup>
-        <Select>
+        <Select value={category} onChange={changeCategory}>
           <option value="Instagram">Instagram</option>
           <option value="Facebook">Facebook</option>
         </Select>
