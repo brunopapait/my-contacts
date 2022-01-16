8da7ff3b962f18307729be77850248de8e5af4a0
brunopapait <brunohpapait@gmail.com>
2022-01-16 01:33:14 -0300

    feat: render contacts in Card

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,18 +1,34 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
+import formatPhone from '../../utils/formatPhone';
 import { Card, Container, Header, InputSearchContainer, ListContainer } from './styles';
 
+
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/contacts')
+      .then(async (response) => {
+        const json = await response.json();
+        setContacts(json);
+      })
+      .catch((error) => {
+      });
+  }, []);
+
   return (
     <Container>
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome .." />
+
       </InputSearchContainer>
 
       <Header>
-        <strong>3 contatos</strong>
+        <strong>{contacts.length} {contacts.length === 1 ? 'contato' : 'contatos'}</strong>
         <Link to="/new">Novo contato</Link>
       </Header>
 
@@ -23,76 +39,34 @@ export default function Home() {
             <img src={arrow} alt="Arrow" />
           </button>
         </header>
+        {
+          contacts.map(item => (
+            <Card key={item.id}>
+              <div className="info">
+                <div className="contact-name">
+                  <strong>{item.name}</strong>
+                  {item.category_name && <small>{item.category_name}</small>}
+                </div>
+                <span>{item.email}</span>
+                <span>{formatPhone(item.phone)}</span>
+              </div>
+              <div className="actions">
+                <Link to={`/edit/${item.id}`}>
+                  <img src={edit} alt="Editar" />
+                </Link>
+                <button type="button">
+                  <img src={trash} alt="Deletar" />
+                </button>
+              </div>
+            </Card>
+          ))
+        }
 
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Bruno H. Papait</strong>
-              <small>Facebook</small>
-            </div>
-            <span>brunohpapait@gmail.com</span>
-            <span>(44) 99863-9827</span>
-          </div>
-          <div className="actions">
-            <Link to="/edit/12">
-              <img src={edit} alt="Editar" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Deletar" />
-            </button>
-          </div>
-        </Card>
 
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Bruno H. Papait</strong>
-              <small>Facebook</small>
-            </div>
-            <span>brunohpapait@gmail.com</span>
-            <span>(44) 99863-9827</span>
-          </div>
-          <div className="actions">
-            <Link to="/edit/12">
-              <img src={edit} alt="Editar" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Deletar" />
-            </button>
-          </div>
-        </Card>
-
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Bruno H. Papait</strong>
-              <small>Facebook</small>
-            </div>
-            <span>brunohpapait@gmail.com</span>
-            <span>(44) 99863-9827</span>
-          </div>
-          <div className="actions">
-            <Link to="/edit/12">
-              <img src={edit} alt="Editar" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Deletar" />
-            </button>
-          </div>
-        </Card>
       </ListContainer>
     </Container>
   );
 }
-
-fetch('http://localhost:3333/contacts')
-  .then(async (response) => {
-    const json = await response.json();
-    console.log(json);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
 
 // SOP -> Same Origin Police -> Politica de mesma origem
 // CORS -> Cross-Origin Resource Sharing -> Compartilhamento de recursos entre
