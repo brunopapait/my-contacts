f4d37373bd26afb2ea551be367c4d1c08f646977
brunopapait <brunohpapait@gmail.com>
2022-01-19 23:40:32 -0300

    feat: add service layer

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -4,9 +4,15 @@ import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import Loader from '../../components/Loader';
-import delay from '../../utils/delay';
+import ContactsService from '../../services/ContactsService';
 import formatPhone from '../../utils/formatPhone';
-import { Card, Container, Header, InputSearchContainer, ListHeader } from './styles';
+import {
+  Card,
+  Container,
+  Header,
+  InputSearchContainer,
+  ListHeader
+} from './styles';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
@@ -14,7 +20,7 @@ export default function Home() {
   const [searchTerm, setSearchTem] = useState('');
   const [isLoading, setIsLoading] = useState(true);
 
-  const filteredContacts = useMemo(() => contacts.filter(contact => (
+  const filteredContacts = useMemo(() => contacts.filter((contact) => (
     contact.name.toLowerCase().includes(searchTerm.toLowerCase())
   )), [contacts, searchTerm]);
 
@@ -23,17 +29,12 @@ export default function Home() {
       try {
         setIsLoading(true);
 
-      const response = await fetch(
-          `http://localhost:3333/contacts?orderBy=${orderBy}`
-          )
-        await delay(400);
-
-        const json = await response.json();
-        setContacts(json);
+        const contactsList = await ContactsService.listContacts(orderBy);
+        setContacts(contactsList);
       } catch (error) {
         console.log(error);
       } finally {
-        setIsLoading(prevState => !prevState);
+        setIsLoading((prevState) => !prevState);
       }
     }
 
@@ -41,11 +42,11 @@ export default function Home() {
   }, [orderBy]);
 
   function handleToggleOrderBy() {
-    setOrderBy(prevState => prevState === 'asc' ? 'desc' : 'asc');
+    setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
   }
 
   function handleChangeSearchTerm(e) {
-    setSearchTem(e.target.value)
+    setSearchTem(e.target.value);
   }
 
   return (
@@ -54,49 +55,50 @@ export default function Home() {
 
       <InputSearchContainer>
         <input
-          type="text"
-          placeholder="Pesquise pelo nome .."
+          type='text'
+          placeholder='Pesquise pelo nome ..'
           value={searchTerm}
           onChange={handleChangeSearchTerm}
         />
       </InputSearchContainer>
 
       <Header>
-        <strong>{filteredContacts.length} {filteredContacts.length === 1 ? 'contato' : 'contatos'}</strong>
-        <Link to="/new">Novo contato</Link>
+        <strong>
+          {filteredContacts.length}{' '}
+          {filteredContacts.length === 1 ? 'contato' : 'contatos'}
+        </strong>
+        <Link to='/new'>Novo contato</Link>
       </Header>
 
       {filteredContacts.length > 0 && (
         <ListHeader orderBy={orderBy}>
-          <button type="button" onClick={handleToggleOrderBy}>
+          <button type='button' onClick={handleToggleOrderBy}>
             <span>Nome</span>
-            <img src={arrow} alt="Arrow" />
+            <img src={arrow} alt='Arrow' />
           </button>
         </ListHeader>
       )}
 
-        {
-          filteredContacts.map(item => (
-            <Card key={item.id}>
-              <div className="info">
-                <div className="contact-name">
-                  <strong>{item.name}</strong>
-                  {item.category_name && <small>{item.category_name}</small>}
-                </div>
-                <span>{item.email}</span>
-                <span>{formatPhone(item.phone)}</span>
-              </div>
-              <div className="actions">
-                <Link to={`/edit/${item.id}`}>
-                  <img src={edit} alt="Editar" />
-                </Link>
-                <button type="button">
-                  <img src={trash} alt="Deletar" />
-                </button>
-              </div>
-            </Card>
-          ))
-        }
+      {filteredContacts.map((item) => (
+        <Card key={item.id}>
+          <div className='info'>
+            <div className='contact-name'>
+              <strong>{item.name}</strong>
+              {item.category_name && <small>{item.category_name}</small>}
+            </div>
+            <span>{item.email}</span>
+            <span>{formatPhone(item.phone)}</span>
+          </div>
+          <div className='actions'>
+            <Link to={`/edit/${item.id}`}>
+              <img src={edit} alt='Editar' />
+            </Link>
+            <button type='button'>
+              <img src={trash} alt='Deletar' />
+            </button>
+          </div>
+        </Card>
+      ))}
     </Container>
   );
 }
