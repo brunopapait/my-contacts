036772c126a01e9fa305e311819c13dd7da77711
brunopapait <brunohpapait@gmail.com>
2022-03-30 23:05:05 -0300

    feat: add isLoading categories

--- frontend/src/components/FormGroup/index.js
+++ frontend/src/components/FormGroup/index.js
@@ -1,23 +1,26 @@
 import PropTypes from 'prop-types';
 import { Container } from './styles';
 
-export default function FormGroup({ children, error }) {
+export default function FormGroup({ children, error, isLoading }) {
   return (
     <Container>
-      {children}
-      {
-        error
-        && <small>{error}</small>
-      }
+      <div className="form-item">
+        {children}
+
+        {isLoading && <div className='loader' />}
+      </div>
+      {error && <small>{error}</small>}
     </Container>
   );
 }
 
 FormGroup.propTypes = {
   children: PropTypes.node.isRequired,
-  error: PropTypes.string
+  error: PropTypes.string,
+  isLoading: PropTypes.bool
 };
 
 FormGroup.defaultProps = {
-  error: null
+  error: null,
+  isLoading: false
 };
