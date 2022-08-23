c900b642f7728ba05b647db77260acfce5cbac39
brunopapait <brunohpapait@gmail.com>
2022-08-22 22:17:36 -0300

    feat: add component children in toastContainer

--- frontend/src/components/Toast/ToastContainer/index.js
+++ frontend/src/components/Toast/ToastContainer/index.js
@@ -1,7 +1,12 @@
+import ToastMessage from '../ToastMessage';
 import { Container } from './styles';
 
 export default function ToastContainer() {
   return (
-    <Container>ToastContainer</Container>
+    <Container>
+      <ToastMessage text='Default toast' />
+      <ToastMessage text='Error toast' type='danger' />
+      <ToastMessage text='Success toast' type='success' />
+    </Container>
   );
 }
