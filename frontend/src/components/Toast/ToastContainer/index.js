e354f45f3b1ec264f02619b55337313a9ae6c7b6
brunopapait <brunohpapait@gmail.com>
2022-08-22 22:55:44 -0300

    feat: refactor code

--- frontend/src/components/Toast/ToastContainer/index.js
+++ frontend/src/components/Toast/ToastContainer/index.js
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import ToastMessage from '../ToastMessage';
 import { Container } from './styles';
 
 export default function ToastContainer() {
+  const [messages] = useState([
+    { id: Math.random(), type: 'default', text: 'Default toast' },
+    { id: Math.random(), type: 'success', text: 'Success toast' },
+    { id: Math.random(), type: 'danger', text: 'Error toast' }
+  ]);
+
   return (
     <Container>
-      <ToastMessage text='Default toast' />
-      <ToastMessage text='Error toast' type='danger' />
-      <ToastMessage text='Success toast' type='success' />
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          text={message.text}
+          type={message.type}
+        />
+      ))}
     </Container>
   );
 }
