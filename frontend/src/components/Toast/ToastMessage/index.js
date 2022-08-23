9828a787e45ab1b6d0d39ce3f30afe89da34f524
brunopapait <brunohpapait@gmail.com>
2022-08-22 22:17:08 -0300

    feat: create file css, component ToastMessage

---
+++ frontend/src/components/Toast/ToastMessage/index.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+import { Container } from './styles';
+import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
+import xCheckIcon from '../../../assets/images/icons/check-circle.svg';
+
+export default function ToastMessage({ text, type }) {
+  return (
+    <Container>
+      {type === 'danger' && <img src={xCircleIcon} alt="X" />}
+      {type === 'success' && <img src={xCheckIcon} alt="Check" />}
+      <strong>{text}</strong>
+    </Container>
+  );
+}
+
+ToastMessage.propTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['success', 'danger', 'default'])
+};
+
+ToastMessage.defaultProps = {
+  type: 'default'
+}
