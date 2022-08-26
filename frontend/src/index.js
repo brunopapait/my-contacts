3eebc48a1950b315f66fea05a75b847f0b52c0c3
brunopapait <brunohpapait@gmail.com>
2022-08-25 23:06:25 -0300

    feat: refactor code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './lib/EventManager';
 
 ReactDOM.render(
   <StrictMode>
--- frontend/src/lib/EventManager.js
+++ frontend/src/lib/EventManager.js
@@ -29,24 +29,3 @@ export default class EventManager {
     this.listeners.set(event, filteredListeners);
   }
 }
-
-const toastEventManager = new EventManager();
-
-function addToast1(payload) {
-  console.log('addToast listener 1 -> ', payload);
-}
-
-function addToast2(payload) {
-  console.log('addToast listener 2 -> ', payload);
-}
-
-toastEventManager.on('addtoast', addToast1);
-toastEventManager.on('addtoast', addToast2);
-
-toastEventManager.emit('addtoast', { type: 'success', text: 'Hello World!' });
-
-toastEventManager.removeListener('addtoast', addToast1);
-
-toastEventManager.emit('addtoast', { type: 'danger', text: 'Depois de remover' });
-
-console.log(toastEventManager);
