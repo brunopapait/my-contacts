764ed0197b7a4eea9f168b9e60e6c9b8c4915368
brunopapait <brunohpapait@gmail.com>
2022-08-27 22:52:41 -0300

    feat: refactor code and remove toast

--- frontend/src/components/Toast/ToastContainer/styles.js
+++ frontend/src/components/Toast/ToastContainer/styles.js
@@ -6,4 +6,5 @@ export const Container = styled.div`
   left: 50%;
   z-index: 2;
   transform: translateX(-50%);
+  cursor: pointer;
 `;
--- frontend/src/components/Toast/ToastMessage/index.js
+++ frontend/src/components/Toast/ToastMessage/index.js
@@ -4,21 +4,25 @@ import { Container } from './styles';
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 import xCheckIcon from '../../../assets/images/icons/check-circle.svg';
 
-export default function ToastMessage({ text, type }) {
+export default function ToastMessage({ message, onRemoveMessage }) {
+  function handleRemoveToast() {
+    onRemoveMessage(message.id);
+  }
+
   return (
-    <Container type={type}>
-      {type === 'danger' && <img src={xCircleIcon} alt="X" />}
-      {type === 'success' && <img src={xCheckIcon} alt="Check" />}
-      <strong>{text}</strong>
+    <Container type={message.type} onClick={handleRemoveToast}>
+      {message.type === 'danger' && <img src={xCircleIcon} alt="X" />}
+      {message.type === 'success' && <img src={xCheckIcon} alt="Check" />}
+      <strong>{message.text}</strong>
     </Container>
   );
 }
 
 ToastMessage.propTypes = {
-  text: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(['success', 'danger', 'default'])
+  message: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['success', 'danger', 'default'])
+  }).isRequired,
+  onRemoveMessage: PropTypes.func.isRequired
 };
-
-ToastMessage.defaultProps = {
-  type: 'default'
-}
