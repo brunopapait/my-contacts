09d4ac5c2a3eb67fd7bad35d9c5e056f7042fc92
brunopapait <brunohpapait@gmail.com>
2022-09-07 21:56:06 -0300

    feat: request to get one contact by id

--- frontend/src/pages/EditContact/index.js
+++ frontend/src/pages/EditContact/index.js
@@ -1,11 +1,40 @@
+import { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
+import Loader from '../../components/Loader';
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+        console.log(contactData);
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({ type: 'danger', text: 'Contato não encontrado !' });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit(formData) {
+    console.log(formData);
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
       <PageHeader title="Editar Bruno Henrique Papait" />
-      <ContactForm buttonLabel="Salvar alterações" />
+      <ContactForm buttonLabel="Salvar alterações" onSubmit={handleSubmit} />
     </>
   );
 }
