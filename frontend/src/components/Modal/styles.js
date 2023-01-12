f945a8de99fcbc9083101fd4c4346b69e50a2e61
brunopapait <brunohpapait@gmail.com>
2023-01-11 22:43:23 -0300

    feat: create animation in modal when open the modal

--- frontend/src/components/Modal/styles.js
+++ frontend/src/components/Modal/styles.js
@@ -1,4 +1,24 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from { /** estilos iniciais */
+    opacity: 0;
+  }
+
+  to { /** estilos finais */
+    opacity: 1;
+  }
+`;
+
+const scaleIn = keyframes`
+  from { /** estilos iniciais */
+    transform: scale(0);
+  }
+
+  to { /** estilos finais */
+    transform: scale(1);
+  }
+`;
 
 export const Overlay = styled.div`
   background: rgba(0, 0, 0, 0.6);
@@ -11,6 +31,7 @@ export const Overlay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  animation: ${fadeIn} 0.3s;
 `;
 
 export const Container = styled.div`
@@ -20,6 +41,7 @@ export const Container = styled.div`
   border-radius: 4px;
   padding: 24px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  animation: ${scaleIn} 0.3s;
 
 
   // '>' -> Significa que vai pegar apenas o elemento que é filho direto de algum elemento.
